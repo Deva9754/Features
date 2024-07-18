8f2d3b19c1a75c60b8db43b541045df1418956cf
Deva9754 <deva.sahu2001@gmail.com>
2024-07-18 15:01:13 +0530

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import Menu from "./components/Animatedtab/Menu";
 import Card from "./components/card/Card";
+import CircularLink from "./components/circular-link-menu/CircularLink";
+import ToggleSwitch from "./components/toggleSwitch/ToggleSwitch";
 
 function App() {
   return (
     <div className="App">
       <Menu />
       <Card />
+      <ToggleSwitch />
+      <CircularLink />
     </div>
   );
 }
