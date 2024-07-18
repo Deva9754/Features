1337acef6054b5233d0882723394b9dd7c0b4b97
Deva9754 <deva.sahu2001@gmail.com>
2024-07-18 12:15:06 +0530

    route fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Menu from "./components/Animatedtab/Menu";
+import Card from "./components/card/Card";
 
 function App() {
   return (
     <div className="App">
       <Menu />
+      <Card />
     </div>
   );
 }
