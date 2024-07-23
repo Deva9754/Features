0f4b8ee1253a6267ab954ad2a16b61223ef024b1
Deva9754 <deva.sahu2001@gmail.com>
2024-07-23 13:35:08 +0530

    carousel jsx

---
+++ src/components/carousel/Carousel.jsx
@@ -0,0 +1,120 @@
+import "./Carousel.css";
+
+const Carousel = () => {
+  return (
+    <>
+      <div className="carousel-container">
+        <div class="slider">
+          <div className="slide-track">
+            <div className="slide">
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
+                alt="Image01"
+                height="100"
+                width="250"
+              />
+            </div>
+            <div className="slide">
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png"
+                alt="Image01"
+                height="100"
+                width="250"
+              />
+            </div>
+            <div className="slide">
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png"
+                alt="Image01"
+                height="100"
+                width="250"
+              />
+            </div>
+            <div className="slide">
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png"
+                alt="Image01"
+                height="100"
+                width="250"
+              />
+            </div>
+            <div className="slide">
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png"
+                alt="Image01"
+                height="100"
+                width="250"
+              />
+            </div>
+            <div className="slide">
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png"
+                alt="Image01"
+                height="100"
+                width="250"
+              />
+            </div>
+            <div className="slide">
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png"
+                alt="Image01"
+                height="100"
+                width="250"
+              />
+            </div>
+            <div className="slide">
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
+                alt="Image01"
+                height="100"
+                width="250"
+              />
+            </div>
+            <div className="slide">
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png"
+                alt="Image01"
+                height="100"
+                width="250"
+              />
+            </div>
+            <div className="slide">
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png"
+                alt="Image01"
+                height="100"
+                width="250"
+              />
+            </div>
+            <div className="slide">
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png"
+                alt="Image01"
+                height="100"
+                width="250"
+              />
+            </div>
+            <div className="slide">
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png"
+                alt="Image01"
+                height="100"
+                width="250"
+              />
+            </div>
+            <div className="slide">
+              <img
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png"
+                alt="Image01"
+                height="100"
+                width="250"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Carousel;
