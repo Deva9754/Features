4a84260c664e58e6826b622028e45920489d1408
Deva9754 <deva.sahu2001@gmail.com>
2024-07-23 16:14:30 +0530

    navigation fix

--- src/components/Animatedtab/Menu.jsx
+++ src/components/Animatedtab/Menu.jsx
@@ -3,39 +3,39 @@ import "./Menu.css";
 const Menu = () => {
   return (
     <>
-      <div class="container">
-        <nav class="nav">
+      <div className="container">
+        <nav className="nav">
           <input type="checkbox" id="menu" />
           <label for="menu">Menu</label>
-          <ul class="menu">
+          <ul className="menu">
             <li>
               <a href="#0">
                 <span>About</span>
-                <i class="ri-user-location-line"></i>
+                <i className="ri-user-location-line"></i>
               </a>
             </li>
             <li>
               <a href="#0">
                 <span>Projects</span>
-                <i class="ri-list-check-3"></i>
+                <i className="ri-list-check-3"></i>
               </a>
             </li>
             <li>
               <a href="#0">
                 <span>Clients</span>
-                <i class="ri-team-line"></i>
+                <i className="ri-team-line"></i>
               </a>
             </li>
             <li>
               <a href="#0">
                 <span>Contact</span>
-                <i class="ri-contacts-line"></i>
+                <i className="ri-contacts-line"></i>
               </a>
             </li>
           </ul>
         </nav>
       </div>
-      <p class="notification">Click on the Menu</p>
+      <p className="notification">Click on the Menu</p>
     </>
   );
 };
