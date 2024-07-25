78b9935bc59ae3d51cac0fee7a78f582c629468c
Deva9754 <deva.sahu2001@gmail.com>
2024-07-25 12:02:18 +0530

    fix card changed

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,13 +3,13 @@ const Card = () => {
   return (
     <>
       <h2>Cards </h2>
-      <div class="wrapper">
-        <div class="card">
+      <div className="wrapper">
+        <div className="card">
           <img
             src="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
             alt="images_loading"
           />
-          <div class="info">
+          <div className="info">
             <h1>Mountain</h1>
             <p>
               Big, tall mountains look really cool and make nature super
@@ -18,12 +18,12 @@ const Card = () => {
             <button>Read More</button>
           </div>
         </div>
-        <div class="card">
+        <div className="card">
           <img
             src="https://images.unsplash.com/photo-1425342605259-25d80e320565?auto=format&fit=crop&w=750&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
             alt="images"
           />
-          <div class="info">
+          <div className="info">
             <h1>Road</h1>
             <p>
               Roads pave the way for journeys, connecting destinations and
@@ -32,12 +32,12 @@ const Card = () => {
             <button>Read More</button>
           </div>
         </div>
-        <div class="card">
+        <div className="card">
           <img
             src="https://images.unsplash.com/photo-1503249023995-51b0f3778ccf?auto=format&fit=crop&w=311&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
             alt="images_loading"
           />
-          <div class="info">
+          <div className="info">
             <h1>Protester</h1>
             <p>
               Protesters voice their concerns, demanding change and standing up
